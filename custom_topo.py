f30ea5cb9abe919d62f46ddc39b58a3bb7cdf104
andygwhite <None>
2022-11-28 06:58:52 -0800

    Modified experiment to handle prediction labels

--- custom_topo.py
+++ custom_topo.py
@@ -5,7 +5,10 @@ from mininet.topo import SingleSwitchTopo
 import json
 import sys
 import subprocess
+import os
 
+# Where to read/write experiment results
+RYU_LOGFILE = './ryu.log'
 # TODO: Add Logging!
 
 class SmartSwitchTopo(SingleSwitchTopo):
@@ -37,6 +40,7 @@ class SmartSwitchTopo(SingleSwitchTopo):
             print('\t1: Open CLI')
             print('\t2: Generate traffic')
             print('\t3: Open xterms for all machines')
+            print('\t4: Run experiment')
             try:
                 choice = input()
             except EOFError:
@@ -49,14 +53,50 @@ class SmartSwitchTopo(SingleSwitchTopo):
                 self.generate_traffic()
             if choice == '3':
                 CLI(self.net, stdin=sys.stdin, script="./open_xterms.sh")
+            if choice == '4':
+                print(self.run_experiment())
             else:
                 print(choice, 'is not a valid option')
 
-    def generate_traffic(self):
+    def generate_traffic(self, pcap_file=None):
         """Generates traffic and listens on target machines"""
+        if not pcap_file:
+            pcap_file = self.cfg["default_pcap_file"]
         p = self.hosts[3].popen([
-                'python3', './packet_generator.py'])
-        print(p.returncode)
+                'python3', './packet_generator.py', self.cfg["default_pcap_file"]])
+        # return p.returncode
+
+    def run_experiment(self):
+        """Generates traffic from each of the labeled PCAP files"""
+        results = dict()
+        for exp, fname in self.cfg["experiment_files"].items():
+            # Clear the ryu log
+            with open(RYU_LOGFILE, 'w') as f:
+                f.write('')
+            print(f"Running {exp} experiment from {fname}")
+            results[exp] = dict()
+            if not os.path.exists(fname):
+                print("ERROR: Could not find file", fname)
+                continue
+            p = self.hosts[3].cmd([
+                'python3', './packet_generator.py', fname, '-d1'])
+            for prediction_result in ["cpu", "network", "memory"]:
+                p = subprocess.Popen(f'grep "PREDICTION: {prediction_result}" ./ryu.log | wc -l', shell=True, stdout=subprocess.PIPE)
+                results[exp][prediction_result] = int(p.communicate()[0])
+        return results
+
+    def read_experiment_results(self):
+        """Uses GREP instead of readlines (performance optimization)
+        to count the predictions made by the controller."""
+        ret = []
+        if not os.path.exists('./ryu.log'):
+            print("Error: Could not find ryu log!")
+            return False
+        for label in ["cpu", "memory", "network"]:
+            # TODO: Remove shell=True
+            p = subprocess.Popen(f'grep "PREDICTION: {label}" ./ryu.log | wc -l', shell=True, stdout=subprocess.PIPE)
+            ret.append(int(p.communicate()[0]))
+        return tuple(ret)
 
     def quit(self):
         print('Shutting Down Topology')
